84552f7474d8e5f22e56b7719106862ec67821ac
strive_gf@163.com <strive_gf@163.com>
2020-11-25 10:36:14 +0800

    微服务入门程序，搭建eureka-server工程

---
+++ micro_service_spring_cloud_parent/demo03_register_center_eureka_10001/src/main/java/com/xgf/demo03_register_center_eureka/Demo03RegisterCenterEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.xgf.demo03_register_center_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer   //表示作为一台服务器启动
+public class Demo03RegisterCenterEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo03RegisterCenterEurekaApplication.class, args);
+    }
+
+}
---
+++ micro_service_spring_cloud_parent/demo03_register_center_eureka_10001/src/test/java/com/xgf/demo03_register_center_eureka/Demo03RegisterCenterEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgf.demo03_register_center_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo03RegisterCenterEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
